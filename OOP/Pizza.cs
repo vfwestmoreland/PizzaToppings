52264f58223105617fdb97ccdd6b4049c58b24c9
vfwes <vfwes@VFWIII>
2022-08-03 11:03:54 -0500

    Add project files.

---
+++ OOP/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaToppings
+{
+    public class Pizza
+    {
+        public List<string> Toppings { get; set; } 
+        
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,45 @@
+﻿using PizzaToppings;
+using Newtonsoft.Json;
+
+
+namespace FavoritePizzaToppings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = File.ReadAllText("C:\\Users\\vfwes\\Capstone-Backend\\C#\\PizzaToppings\\OOP\\pizzas.json");
+            var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(result);
+
+            Dictionary<String, int> totalToppings = new();
+
+            pizzas.ForEach(pizza =>
+            {
+
+                string toppingsAsString = String.Join(",", pizza.Toppings.OrderByDescending(x => x));
+
+                if (!totalToppings.ContainsKey(toppingsAsString))
+                {
+                    totalToppings[toppingsAsString] = 1;
+                }
+                else
+                {
+                    totalToppings[toppingsAsString]++;
+                };
+
+            });
+
+            var mostPopularPizzaToppings = totalToppings.OrderByDescending(toppingCombo => toppingCombo.Value).Take(1).FirstOrDefault();
+            var mostTimesOrdered = totalToppings.Max(item => item.Value);
+            Console.WriteLine($"The most popular pizza was ordered {mostTimesOrdered} times.");
+            Console.WriteLine($"The most popular pizza is {mostPopularPizzaToppings.Key}, and it was ordered {mostPopularPizzaToppings.Value} times.");
+
+
+            Console.ReadLine();
+
+            
+
+
+        }
+    }
+}
